fa97ec4dbcf9fb643d087cc584440142aac7145c
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-12-02 07:57:03 +0000

    Update nordiska.py

--- nordiska.py
+++ nordiska.py
@@ -1,6 +1,17 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
+def makeRoof(x,z,y):
+    for h in range(0,12):
+        mc.setBlocks(x+h,z+h  ,y+h    ,x+22-h  ,z+h,y+42-h   ,17)
+        if h < 11:
+            mc.setBlocks(x+h+1,z+h,y+h+1  ,x+22-1-h,z+h,y+42-h-1 , 0)
+  
+def makeStairs(x,z,y,numstairs,width): 
+    for s in range(0, numstairs):
+        for w in range(0,width):
+             mc.setBlock(x+w,z+s,y+s,1)#stone y-koordinate
+    
 position = mc.player.getTilePos()
 
 
@@ -19,18 +30,24 @@ mc.setBlock(90,4,20 ,0)     #toilet window boys
 mc.setBlock(93,4,20 ,0)     #toilet window girls
 
 mc.setBlocks(81,6,21, 99,6,59   ,1) #first floor
-mc.setBlocks(81,6,21, 83,6,29   ,0) #air for stairs
-
-#stairs ground floor
-h=0
-for y in range(23, 30):
-     mc.setBlock(81,h,y,1)#stone y-koordinate
-     mc.setBlock(82,h,y,1)#stone y-koordinate
-     mc.setBlock(83,h,y,1)#stone y-koordinate     
-     h=h+1
 
 
-mc.player.setTilePos(90, 1, 0)
+mc.setBlocks(81,6,21  ,83,6,29   ,0) #air for groundfloor stairs
+makeStairs(81,0,23 ,7,3) #ground floor stairs
+
+makeRoof(79,13,19)
+
+mc.setBlocks(85,12,34 ,87,12,38   ,0) #air for secondfloor stairs
+makeStairs(85,7,33 ,6,3) #stairs to second floor
+
+
+
+
+
+
+
+
+mc.player.setTilePos(90, 1, 15)
 
 #make coordinate system
 for x in range(0, 10):
@@ -41,4 +58,3 @@ for z in range(0, 10):
 
 for y in range(0, 10):
      mc.setBlock(0,1,y,1)#stone y-koordinate
-
