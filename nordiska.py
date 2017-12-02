b0fda1da59596f970fbb60c22de6e59ee9f1e731
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-12-02 09:28:03 +0000

    Update nordiska.py

--- nordiska.py
+++ nordiska.py
@@ -10,11 +10,11 @@ def makeRoof(x,z,y):
 def makeStairs(x,z,y,numstairs,width): 
     for s in range(0, numstairs):
         for w in range(0,width):
-             mc.setBlock(x+w,z+s,y+s,1)#stone y-koordinate
+             mc.setBlock(x+w,z+s,y+s,17)#stone y-koordinate
 
 def makeTree(x,z,y):
         mc.setBlocks(x,z,y       ,x+1,z+6,y+1,  17)
-        mc.setBlocks(x-5,z+6,y-5 ,x+4,z+26,y+5, 18)
+        mc.setBlocks(x-3,z+6,y-4 ,x+4,z+26,y+5, 18)
 
 def printCoordinateSystem():
     for x in range(0, 10):
@@ -37,6 +37,35 @@ mc.setBlocks(-150,-1,0,150,0,-20,1)      #stone=street
 mc.setBlocks(80,0,20,100,12,60  ,24)     #NORDISKA first and second floor
 mc.setBlocks(81,1,21,  99,11,59  ,0)     #air in NORDISKA
 mc.setBlocks(84,1,20,   87,4,21  ,0)     #front door opening
+mc.setBlocks(80,7,20,   100,11,24,0)     #front balcony opening
+mc.setBlocks(80,7,57,   100,11,60,0)     #lake  balcony opening
+
+
+
+
+mc.setBlocks( 80,7,60,  100,7,60,  24)   #lake balcony walls
+mc.setBlocks( 80,7,56,   80,7,60,  24)
+mc.setBlocks(100,7,56,  100,7,60,  24)  
+
+mc.setBlocks( 80,8,60,  80,11,60,  73)   #pilar lake balcony
+mc.setBlocks( 84,8,60,  84,11,60,  73)   #pilar lake balcony
+mc.setBlocks( 88,8,60,  88,11,60,  73)   #pilar lake balcony
+mc.setBlocks( 92,8,60,  92,11,60,  73)   #pilar lake balcony
+mc.setBlocks( 96,8,60,  96,11,60,  73)   #pilar lake balcony
+mc.setBlocks(100,8,60, 100,11,60,  73)   #pilar lake balcony
+
+
+mc.setBlocks( 80,7,20,  100,7,20,  24)   #front balcony walls
+mc.setBlocks( 80,7,20,   80,7,24,  24)
+mc.setBlocks(100,7,20,  100,7,24,  24)
+
+mc.setBlocks( 80,8,20,  80,11,20,  73)   #pilar front balcony
+mc.setBlocks( 84,8,20,  84,11,20,  73)   #pilar front balcony
+mc.setBlocks( 88,8,20,  88,11,20,  73)   #pilar front balcony
+mc.setBlocks( 92,8,20,  92,11,20,  73)   #pilar front balcony
+mc.setBlocks( 96,8,20,  96,11,20,  73)   #pilar front balcony
+mc.setBlocks(100,8,20, 100,11,20,  73)   #pilar front balcony
+
 
 mc.setBlock(90,4,20 ,0)     #toilet window boys
 mc.setBlock(93,4,20 ,0)     #toilet window girls
@@ -47,29 +76,30 @@ mc.setBlocks(81,6,21, 99,6,59   ,1) #first floor
 mc.setBlocks(81,6,21  ,83,6,29   ,0) #air for groundfloor stairs
 makeStairs(81,0,23 ,7,3) #ground floor stairs
 
+mc.setBlock(81,4,22,50) #torch at entry
+mc.setBlock(81,5,23,50) #torch at entry
+mc.setBlock(81,6,24,50) #torch at entry
+mc.setBlock(81,7,25,50) #torch at entry
+mc.setBlock(81,8,26,50) #torch at entry
+mc.setBlock(81,9,27,50) #torch at entry
+
 makeRoof(79,13,19)
 
 mc.setBlocks(85,12,34 ,87,12,38   ,0) #air for secondfloor stairs
 makeStairs(85,7,33 ,6,3) #stairs to second floor
 
 
-
 for x in range(0,100):  #fence to the street
-    mc.setBlock(x,1,0  ,17)
-    mc.setBlock(x,2,0  ,17)
+    mc.setBlock(x,1,0  ,85)
+    mc.setBlock(x,2,0  ,85)
 
-makeTree(106,0,60)
-makeTree(106,0,75)
-makeTree(60,0,70)
+makeTree(96,0,75)
 
 for x in range(0,8):   #trees at the street
     makeTree(x*13,0,4)
 
-
 for x in range(0,8):   #trees at the parking space
     makeTree(x*13,0,-20)
-
-
 
 
 
